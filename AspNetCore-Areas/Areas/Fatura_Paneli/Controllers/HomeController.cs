b3c707c85d866c673e02305b02e912f52677af18
MrAzimzadeh <mehemmedezimzase@gmail.com>
2023-04-05 21:25:48 +0400

    Add project files.

---
+++ AspNetCore-Areas/Areas/Fatura_Paneli/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_Areas.Areas.Fatura_Paneli.Controllers
+{
+    [Area(nameof(Fatura_Paneli))]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = TempData["Data"];
+            return View();
+        }
+    }
+}
---
+++ AspNetCore-Areas/Areas/Yonetim_Panel/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_Areas.Areas.Yonetim_Panel.Controllers
+{
+
+
+    [Area(nameof(Yonetim_Panel))]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            TempData["Data"] = "Salam bu bir temp data mesajidir ";
+            return RedirectToAction("Index", "Home", new { area = "Fatura_Paneli" });
+        
+        }
+    }
+}
